b39013fc0d381dc3d5fe32f4ba7570f0c6a6d858
vonmecheln <luishenrique.si@gmail.com>
2024-08-02 21:39:58 +0000

    Deploy website - based on 9bf0f2ae66a3e20aa98baad2bcc55f6774fe8553

---
+++ assets/js/083cfad2.3b48887b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkifpr_horarios=self.webpackChunkifpr_horarios||[]).push([[60824],{99186:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/ifpr-horarios/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- versioned_docs/version-2024.1.11/turma/fpw/fpw.md
+++
@@ -1,5 +0,0 @@
-# FPW
-
-## FIC Desenvolvimento de sites para internet (156h)
-
-- [FPW2024-1](fpw2024-1)
